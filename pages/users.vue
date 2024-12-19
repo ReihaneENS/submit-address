365bf9a09fc6b9eb1195d9901aa2bfc30412763f
ReihaneENS <ebrahiminsb@gmail.com>
2024-12-19 15:40:50 +0330

    feat : add users page

---
+++ pages/users.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="users-wrapper">
+    <div class="inner">
+      <h4>آدرس ها و مشخصات</h4>
+      <UserCard
+          firstName="محمد"
+          lastName="محمدی"
+          phoneNumber="۰۹۱۲۱۲۳۴۵۶۷"
+          tellPhone="۰۲۱۳۴۵۶۷۸۹"
+          gender="آقا"
+          address="لورم ایپسوم متن ساختگی با تولید سادگی "
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.users-wrapper{
+  padding: 50px 0;
+  position: relative;
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .inner {
+    width: 60%;
+
+    h4 {
+      margin-bottom: 20px;
+    }
+  }
+  /****************************/
+  /*     Responsive Design    */
+  /****************************/
+  @media screen and (max-width: 1081px){
+    .inner{
+      width: 90%;
+    }
+  }
+}
+</style>
